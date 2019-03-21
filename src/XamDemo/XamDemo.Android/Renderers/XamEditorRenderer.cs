2e6c4f64a3e257461359d61c7513b4a5c16428ae
Michael <vaiyanzi@gmail.com>
2019-03-21 18:37:27 +0800

    created the repository.

---
+++ src/XamDemo/XamDemo.Android/Renderers/XamEditorRenderer.cs
@@ -0,0 +1,30 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamDemo.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Editor), typeof(XamEditorRenderer))]
+namespace XamDemo.Droid.Renderers
+{
+    public class XamEditorRenderer : EditorRenderer {
+        public XamEditorRenderer(Context context):base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
+            this.Control.Background = null;
+            IntPtr intPtrTextViewClass = JNIEnv.FindClass(typeof(TextView));
+            IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(intPtrTextViewClass, "mCursorDrawableRes", "I");
+            JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty, Resource.Drawable.color_cursor);
+            this.Control.Background = Context.GetDrawable(Resource.Drawable.editor_bg);
+            this.Control.Gravity = Android.Views.GravityFlags.CenterVertical;
+            this.Control.SetCompoundDrawablesWithIntrinsicBounds(null, null, Context.GetDrawable(Resource.Drawable.abc_ic_arrow_drop_right_black_24dp), null);
+            
+        }
+    }
+
+}
---
+++ src/XamDemo/XamDemo.Android/Renderers/XamEntryRenderer.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Android.Runtime;
+using Android.Widget;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+using XamDemo.Droid.Renderers;
+
+[assembly: ExportRenderer(typeof(Entry), typeof(XamEntryRenderer))]
+namespace XamDemo.Droid.Renderers
+{
+    public class XamEntryRenderer: EntryRenderer
+    {
+        public XamEntryRenderer(Context context):base(context)
+        {           
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                this.Control.Background = null;
+                IntPtr intPtrTextViewClass = JNIEnv.FindClass(typeof(TextView));
+                IntPtr mCursorDrawableResProperty = JNIEnv.GetFieldID(intPtrTextViewClass, "mCursorDrawableRes", "I");
+                JNIEnv.SetField(Control.Handle, mCursorDrawableResProperty,Resource.Drawable.color_cursor);
+                this.Control.Background = Context.GetDrawable(Resource.Drawable.editor_bg);
+                this.Control.Gravity = Android.Views.GravityFlags.CenterVertical;
+            }
+        }
+    }
+
+}
